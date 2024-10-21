f0fcd5af150c569179e541090bbc612460bfa5ae
Tytan-Codes <tytan_official@proton.me>
2024-10-21 17:15:18 -0400

    chnaged nothing i dont know why it wants this

--- components/ui/narbar.tsx
+++ components/ui/narbar.tsx
@@ -3,12 +3,12 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 const Navbar = () => { 
     return (
-        <nav className="">
+        <nav>
             <div className="container mx-auto flex justify-between items-center p-2">
                 <h1 className="font-mono text-xl">
                     Tytan Codes
                 </h1>
-                <Button className="flex items-center">
+                <Button variant="outline" className="flex items-center">
                     Menu
                     <HamburgerMenuIcon className="ml-2" />
                 </Button>
