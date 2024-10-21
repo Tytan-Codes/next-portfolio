97299d5856795cf869cb7d9203bfca3e128a9fe1
Tytan-Codes <tytan_official@proton.me>
2024-10-21 17:14:56 -0400

    make navbar not show up on all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-"use client"
-import { Navbar } from '@/components/ui/narbar';
 import { ModeToggle } from '@/components/ui/darkmode';
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <div>
         <h1>Home</h1>
         <ModeToggle />
