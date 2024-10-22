e62e29cbe30d1a7a8722263c74b5a5175a643dbd
Tytan-Codes <tytan_official@proton.me>
2024-10-22 15:14:24 -0400

    did stuff

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Home</h1>
-        <ModeToggle />
+      <div className='flex items-center justify-center min-h-screen'>
+        <h1 className='text-8xl font-mono'>
+          Tytan Codes
+        </h1>
       </div>
     </>
   )
