26100a35f302425ee93e2ea98fddf757252a9a75
Tytan-Codes <tytan_official@proton.me>
2024-10-24 13:05:20 -0400

    made a slogan

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-;import { Navbar } from '@/components/narbar';
-import Footer  from "@/components/footer";
+import { Navbar } from '@/components/narbar';
+import Footer from "@/components/footer";
+
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className='flex items-center justify-center min-h-screen text-8xl font-mono'>
-        Tytan Codes
+      <div className='flex flex-col items-center justify-center min-h-screen text-8xl font-mono'>
+        <div>Tytan Codes</div>
+        <h2 className='text-2xl mt-4'>
+          Crafting Stunning Website Designs with Exceptional Development Skills
+        </h2>
       </div>
       <Footer />
     </>
